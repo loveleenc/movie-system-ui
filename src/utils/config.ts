d0d379de521c7ba3944c7274b0a509b8f0557d11
Loveleen C <loveleenchaudhari@gmail.com>
2025-11-15 12:27:33 +0530

    fix: use vite import.meta.env instead of dotenv

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,13 +1,10 @@
-import dotenv from 'dotenv'
-
-dotenv.config({path: './.env'})
 
 let BASE_URL:string = "";
-if(process.env.NODE_ENV === 'production'){
-    BASE_URL = process.env.BASE_URL_PROD ? process.env.BASE_URL_PROD : ""
+if(import.meta.env.DEV){
+    BASE_URL = import.meta.env['VITE_BASE_URL_DEV']
 }
-else if(process.env.NODE_ENV === 'development'){
-    BASE_URL = process.env.BASE_URL_DEV ? process.env.BASE_URL_DEV : ""
+else if(import.meta.env.PROD){
+    BASE_URL = import.meta.env['VITE_BASE_URL_PROD']
 }
 
 export default {
